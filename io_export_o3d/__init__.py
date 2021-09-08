6e3211cb2c18f4c295380cc1ad158e1a6650f06e
Nathan Burnham <nathan@nathanburnham.uk>
2021-09-08 17:06:03 +0000

    Release prep

--- io_export_o3d/__init__.py
+++ io_export_o3d/__init__.py
@@ -38,15 +38,15 @@ bl_info = {
     "name": "O3D Exporter",
     "description": "Export selected object(s) to an OMSI Mesh file",
     "author": "Nathan Burnham (Road-hog123)",
-    "version": (),  # development version
+    "version": (1, 0, 0),
     "blender": (2, 83, 0),
     "location": "File > Export > OMSI Mesh (.o3d)",
-    "warning": "Development version - may contain major bugs!",
+    "warning": "",
     "doc_url": "https://github.com/Road-hog123/blender-omsi-exporter",
     "tracker_url": (
         "https://github.com/Road-hog123/blender-omsi-exporter/issues"
     ),
-    "support": 'TESTING',
+    "support": 'COMMUNITY',
     "category": 'Import-Export',
 }
 
