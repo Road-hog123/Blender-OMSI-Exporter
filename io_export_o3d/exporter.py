70562341029e19f2f382f2ddd6f06a64c0125c07
Nathan Burnham <nathan@nathanburnham.uk>
2022-03-12 10:37:23 +0000

    Fix types with incorrectly non-nullable materials

--- io_export_o3d/exporter.py
+++ io_export_o3d/exporter.py
@@ -28,6 +28,7 @@ from collections.abc import Iterable, Iterator
 from itertools import groupby
 from operator import attrgetter, methodcaller
 from pathlib import Path
+from typing import Optional
 from bpy.types import (
     Object,
     Mesh,
@@ -182,7 +183,7 @@ class Exporter:
                 # clear extracted mesh now we've finished with it
                 obj.to_mesh_clear()
 
-        wrappers: dict[Material, MaterialWrapper] = {}
+        wrappers: dict[Optional[Material], MaterialWrapper] = {}
 
         Vertex = tuple[tuple[float, ...],
                        tuple[float, ...],
@@ -190,7 +191,7 @@ class Exporter:
                        tuple[tuple[str, float], ...]]
 
         vertices: dict[Vertex, int] = _IndexDict()
-        materials: dict[tuple[Material, int], int] = _IndexDict()
+        materials: dict[tuple[Optional[Material], int], int] = _IndexDict()
         bones: defaultdict[str, dict[int, float]] = defaultdict(dict)
 
         def triangles() -> Iterator[tuple[tuple[int, int, int], int]]:
@@ -208,7 +209,7 @@ class Exporter:
                     raise
             # this dictionary keeps track of the material repeat index,
             # which is used when merging materials
-            counter: Counter[Material] = Counter()
+            counter: Counter[Optional[Material]] = Counter()
 
             for me, slots, vertex_groups in meshes():
                 # groupyby requires its input to be sorted
